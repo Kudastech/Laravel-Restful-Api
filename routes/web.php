d5515d5241864323c5dd6575b65d6aeb3f777adf
Kudastech <kudastech@gmail.com>
2024-11-16 13:30:53 +0100

    A new route was create for authentication and creating a token

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('auth', [AuthController::class,'index']);
 
 Route::middleware([
     'auth:sanctum',
